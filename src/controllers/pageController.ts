88681d61deb2c31870cab6a2c7bee1f773d63b44
sirguy12 <ryankairo09@yahoo.com.br>
2023-05-07 17:55:39 -0300

    Controler mostrar imagens

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,55 @@
 import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (req:Request, res:Response) =>{
-
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
-
 export const dogs = (req:Request, res:Response) =>{
+    res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: true,
+            cat: false,
+            fish: false
+        },
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 
 }
 export const cats = (req:Request, res:Response) =>{
-
+    res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: true,
+            fish: false
+        },
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 export const fishes = (req:Request, res:Response) =>{
-
+    res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: false,
+            fish: true
+        },
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
